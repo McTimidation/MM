f07ddeaaa9e08c017da107e46fb281d2a372de84
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-10-17 03:06:29 +0000

    revamping design projects/portfolio

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Header from './header.js';
+import './App.css';
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ src/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+
+    return (
+        <div className="header-section">
+            <div className="header-frame">
+                <h1>Michael McGraw</h1>
+                <h2>Full Stack Web Developer in Lexington, KY</h2>
+            </div>
+            <div className="img-frame">
+                <img id="propic" src={require("./images/mcgraw-michael.jpg")} alt=""></img>
+            </div>
+        </div>
+    )
+}
+
+export default Header;

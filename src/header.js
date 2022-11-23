fe7de39cfb8b7d6c465dc68fe535411bc738efbd
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-11-23 21:13:44 +0000

    fix typos

--- src/header.js
+++ src/header.js
@@ -7,6 +7,7 @@ function Header() {
             <div className="header-frame">
                 <h1>Michael McGraw</h1>
                 <h2>Full Stack Web Developer in Lexington, KY</h2>
+                <h3>Thanks for visiting, my site is still a work in progress. I plan to have it fully finished by mid-December. In the meantime, please get in touch with me on <a href="https://www.linkedin.com/in/michaelmcgraw09/">LinkedIn</a> </h3>
             </div>
             <div className="img-frame">
                 <img id="propic" src={require("./images/mcgraw-michael.jpg")} alt=""></img>
--- src/portfolio.js
+++ src/portfolio.js
@@ -28,7 +28,7 @@ function PortfolioPage() {
             <ProjectCard title={'React Restaurant'} liveDemo={'#'} repoLink={'https://github.com/McTimidation/react-restaurant/tree/main/app/src'} imgsrc={restaurant} text={'A Fake Restaurant page with menus populated from an API'} />
             <ProjectCard title={'Weather App'} liveDemo={'#'} repoLink={'https://github.com/McTimidation/weatherapp/blob/main/js/main.js'} imgsrc={weather} text={'An app that pulls weather data for any zipcode'} />
             <ProjectCard title={'Tic Tac Toe'} liveDemo={'#'} repoLink={'https://github.com/McTimidation/tic-tac-toe/blob/main/script/sandbox.js'} imgsrc={ticTac} text={'A simple game of Tic Tac toe'} />
-            <ProjectCard title={'Randon Story Generator'} liveDemo={'https://github.com/McTimidation/random-story-generator'} repoLink={'#'} imgsrc={randoStory} text={'A fun random story generator'} />
+            <ProjectCard title={'Random Story Generator'} liveDemo={'https://github.com/McTimidation/random-story-generator'} repoLink={'#'} imgsrc={randoStory} text={'A fun random story generator'} />
             <ProjectCard title={'Mind Reader'} liveDemo={'#'} repoLink={'https://github.com/McTimidation/mind-reader'} imgsrc={mindRead} text={'An app that can read your mind!'} />
             <ProjectCard title={'Brewery Locator'} liveDemo={'#'} repoLink={'https://github.com/McTimidation/beer-api'} imgsrc={brewAPI} text={'An app that will show you every brewery for any city'} />
         </div>

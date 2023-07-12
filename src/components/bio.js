c8f54515fe82452e347df792adae1997b8a2985c
Michael McGraw <90709441+McTimidation@users.noreply.github.com>
2023-07-11 20:32:46 -0400

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,7 +8,7 @@ function Bio( { state } ) {
             <p>I am a full stack web developer that loves finding solutions to problems. 
                 Prior to becoming a developer, I worked management and technical maintenance in the escape room industry for 6 years. 
                 My experience in those roles left me with skills to properly debug and troubleshoot issues, 
-                work with a team with different interests and motivations from my own, and find ways to delegate and lean into the strength and wisdom of others around me.
+                work on a team with different interests and motivations from my own, and find ways to delegate and lean into the strength and wisdom of others around me.
             </p>
         </>
     )
